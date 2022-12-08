b2eb9d468b823526a213da3c90f08f6944692b1c
Patrik Devlin <patrikdevlin@gmail.com>
2022-12-08 01:26:28 -0500

    feat: add nvim settings

---
+++ dot_config/nvim/lua/patrikdevlin/packer.lua
@@ -0,0 +1,109 @@
+return require("packer").startup(function(use)
+    -- Packer can manage itself
+    use("wbthomason/packer.nvim")
+
+    -- Plugins
+    use { "windwp/nvim-autopairs" }
+    use { "numToStr/Comment.nvim" }
+    use { "akinsho/bufferline.nvim" }
+    use { "nvim-lualine/lualine.nvim" }
+    use { "nvim-lua/plenary.nvim" }
+    use { "lewis6991/impatient.nvim" }
+    use { "goolord/alpha-nvim" }
+
+    -- ColorSchemes
+	use { "dracula/vim", as = "dracula"}
+
+    --LSP
+    use { "neovim/nvim-lspconfig"} -- enable LSP
+    use { "williamboman/mason.nvim"}
+    use { "williamboman/mason-lspconfig.nvim"}
+    use { "jose-elias-alvarez/null-ls.nvim"} -- formatters and linters
+    use { "RRethy/vim-illuminate"}
+
+    -- Telescope
+    use { "nvim-telescope/telescope.nvim" }
+
+    -- Treesitter
+    use { "nvim-treesitter/nvim-treesitter"}
+
+    -- cmp plugins
+    use { "hrsh7th/nvim-cmp" } -- The completion plugin
+    use { "hrsh7th/cmp-buffer" } -- buffer completions
+    use { "hrsh7th/cmp-path" } -- path completions
+    use { "saadparwaiz1/cmp_luasnip" } -- snippet completions
+    use { "hrsh7th/cmp-nvim-lsp" }
+    use { "hrsh7th/cmp-nvim-lua" }
+
+    -- Git
+    use { "lewis6991/gitsigns.nvim"}
+
+    -- DAP
+    -- use { "mfussenegger/nvim-dap" }
+    -- use { "rcarriga/nvim-dap-ui" }
+    -- use { "ravenxrz/DAPInstall.nvim" } 
+
+	--[[
+    --
+	-- Lazy loading:
+	-- Load on specific commands
+	use {'tpope/vim-dispatch', opt = true, cmd = {'Dispatch', 'Make', 'Focus', 'Start'}}
+	-- Load on an autocommand event
+	use {'andymass/vim-matchup', event = 'VimEnter'}
+
+	-- Load on a combination of conditions: specific filetypes or commands
+	-- Also run code after load (see the "config" key)
+	use {
+	'w0rp/ale',
+	ft = {'sh', 'zsh', 'bash', 'c', 'cpp', 'cmake', 'html', 'markdown', 'racket', 'vim', 'tex'},
+	cmd = 'ALEEnable',
+	config = 'vim.cmd[[ALEEnable]]--[['
+	}
+
+	-- Plugins can have dependencies on other plugins
+	use {
+	'haorenW1025/completion-nvim',
+	opt = true,
+	requires = {{'hrsh7th/vim-vsnip', opt = true}, {'hrsh7th/vim-vsnip-integ', opt = true}}
+	}
+
+	-- Plugins can also depend on rocks from luarocks.org:
+	use {
+	'my/supercoolplugin',
+	rocks = {'lpeg', {'lua-cjson', version = '2.1.0'}}
+	}
+    
+	-- You can specify rocks in isolation
+	use_rocks 'penlight'
+	use_rocks {'lua-resty-http', 'lpeg'}
+
+	-- Local plugins can be included
+	use '~/projects/personal/hover.nvim'
+
+	-- Plugins can have post-install/update hooks
+	use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'}
+
+	-- Post-install/update hook with neovim command
+	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
+	-- Post-install/update hook with call of vimscript function with argument
+	use { 'glacambre/firenvim', run = function() vim.fn['firenvim#install'](0) end }
+
+	-- Use specific branch, dependency and run lua file after load
+	use {
+	'glepnir/galaxyline.nvim', branch = 'main', config = function() require'statusline' end,
+	requires = {'kyazdani42/nvim-web-devicons'}
+	}
+
+	-- Use dependency and run lua function after load
+	use {
+	'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
+	config = function() require('gitsigns').setup() end
+	}
+
+	-- You can specify multiple plugins in a single call
+	use {'tjdevries/colorbuddy.vim', {'nvim-treesitter/nvim-treesitter', opt = true}}
+    
+    -- ]]
+
+end)
