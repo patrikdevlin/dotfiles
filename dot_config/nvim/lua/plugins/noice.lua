1bf0cdce4bee9e3fe619a910a584661efb28e2fd
Patrik Devlin <patrikdevlin@gmail.com>
2024-04-13 15:45:55 -0400

    better noice settings

--- dot_config/nvim/lua/plugins/noice.lua
+++ dot_config/nvim/lua/plugins/noice.lua
@@ -1,30 +1,52 @@
 return {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = {
-        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-        "MunifTanjim/nui.nvim",
-        -- OPTIONAL:
-        --   `nvim-notify` is only needed, if you want to use the notification view.
-        --   If not available, we use `mini` as the fallback
-        "rcarriga/nvim-notify",
-    },
-    keys = {
-        {
-            "<leader>nd",
-            function()
-                require("noice").cmd("dismiss")
-            end,
-            mode = "",
-            desc = "Dismiss notification",
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
         },
-    },
-    opts = {
-        routes = {
+        keys = {
             {
-                view = "notify",
-                filter = { event = "msg_showmode" },
+                "<leader>nd",
+                function()
+                    require("noice").cmd("dismiss")
+                end,
+                mode = "",
+                desc = "Dismiss notification",
+            },
+        },
+        opts = {
+            lsp = {
+                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                override = {
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true,
+                    ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                },
+            },
+            presets = {
+                bottom_search = true,         -- use a classic bottom cmdline for search
+                command_palette = true,       -- position the cmdline and popupmenu together
+                long_message_to_split = true, -- may not need long_message_to_split with wrapped-compact
+                inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+                lsp_doc_border = false,       -- add a border to hover docs and signature help
+            },
+            routes = {
+                {
+                    view = "notify",
+                    filter = { event = "msg_showmode" },
+                },
             },
         },
     },
+    {
+        "rcarriga/nvim-notify",
+        event = "VeryLazy",
+        opts = {
+            timeout = 3500,
+            render = "wrapped-compact",
+            stages = 'static'
+        }
+    }
 }
