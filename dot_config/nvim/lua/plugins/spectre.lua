869663195058424652c4695f014bad0aa36dd5d2
Patrik Devlin <patrikdevlin@gmail.com>
2025-02-21 12:53:06 -0500

    feat: zshrc updates for sed plus nvim config updates

--- dot_config/nvim/lua/plugins/spectre.lua
+++ dot_config/nvim/lua/plugins/spectre.lua
@@ -1,14 +1,14 @@
 return {
-    {
-        "nvim-pack/nvim-spectre",
-        keys = {
-            { "<leader>S", '<cmd>lua require("spectre").toggle()<CR>', { desc = "Toggle Spectre" } },
-            {
-                "<leader>sw",
-                '<cmd>lua require("spectre").open_visual({select_word=true})<CR>',
-                { desc = "Search current word" },
-            },
-        },
-        opts = {},
-    },
+	{
+		"nvim-pack/nvim-spectre",
+		keys = {
+			{ "<leader>S", '<cmd>lua require("spectre").toggle()<CR>', { desc = "Toggle Spectre" } },
+			{
+				"<leader>sw",
+				'<cmd>lua require("spectre").open_visual({select_word=true})<CR>',
+				{ desc = "Search current word" },
+			},
+		},
+		opts = {},
+	},
 }
